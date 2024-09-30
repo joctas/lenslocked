1ebbf8abb80176a0700b4e5a19cb4948c41a2a76
João Octávio Santiago <jomsoutosantiago@gmail.com>
2024-09-30 00:48:52 -0300

    added router via chi for practice

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	fmt.Println("Starting server on: http://localhost:4000")
-	http.ListenAndServe(":4000", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", helloHandler)
+	r.Get("/contacts/{ID}", contactHandler)
+	http.ListenAndServe(":4000", r)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, world!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is for contact with id: %s", r.PathValue("ID"))
 }
